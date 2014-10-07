a1dc86f97a677296e1da931a30f2620f9d188bac
erickipnis <erickipnis@yahoo.com>
2014-10-07 18:03:15 -0400

    Added Player Movement & Fuse Movement
    Added player controls to move in all directions and a fuse that moves in
    a single direction.

---
+++ Assets/Scripts/FuseController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FuseController : MonoBehaviour {
+
+	GameObject fuse;
+	
+	void Start () {
+		fuse = GameObject.FindGameObjectWithTag("Fuse");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector2 position = fuse.transform.position;
+
+		position.x += .1f;
+
+		fuse.transform.position = position;
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour 
+{
+	GameObject player;
+	// Use this for initialization
+	void Start () 
+	{
+		player = GameObject.FindGameObjectWithTag ("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		Vector2 position = player.transform.position;
+
+		if (Input.GetKey(KeyCode.LeftArrow))
+		{
+			position.x -= .1f;
+			player.transform.position = position;
+		} 
+		else if (Input.GetKey(KeyCode.RightArrow)) 
+		{
+			position.x += .1f;
+			player.transform.position = position;
+		}
+		
+		if (Input.GetKey(KeyCode.UpArrow))
+		{
+			position.y += .1f;
+			player.transform.position = position;
+		}
+		else if(Input.GetKey(KeyCode.DownArrow))
+		{
+			position.y -= .1f;
+			player.transform.position = position;
+		}
+
+
+
+	}
+}
