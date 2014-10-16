1759e154c1d20c022a07fe3a404b22019cc021e9
Eric Kipnis <erickipnis@yahoo.com>
2014-10-16 16:42:05 -0400

    Revert "Revert "Added Title Screen and Instructions""
    This reverts commit dc8b58968a1cc21b8a8a6b76a955109e77d17eae.

---
+++ Assets/Scripts/Instructions.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class Instructions : MonoBehaviour
+{
+	private GUISkin skin;
+	
+	void Start()
+	{
+		skin = Resources.Load("TitleGUI") as GUISkin;
+	}
+	
+	void OnGUI()
+	{
+		const int buttonWidth = 200;
+		const int buttonHeight = 65;
+		
+		GUI.skin = skin;
+		
+		if (GUI.Button(new Rect(Screen.width / 2 - (buttonWidth / 2), Screen.height - 80, buttonWidth, buttonHeight), "PLAY"))
+		{
+			Application.LoadLevel("Level1");
+		}
+	}
+}
---
+++ Assets/Scripts/TitleScreen.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class TitleScreen : MonoBehaviour
+{
+	private GUISkin skin;
+	
+	void Start()
+	{
+		skin = Resources.Load("TitleGUI") as GUISkin;
+	}
+	
+	void OnGUI()
+	{
+		const int buttonWidth = 200;
+		const int instrButtonWidth = 400;
+		const int buttonHeight = 65;
+
+		GUI.skin = skin;
+
+		if (GUI.Button(new Rect(Screen.width / 2 - (buttonWidth / 2), (2 * Screen.height / 3) - (buttonHeight / 2), buttonWidth, buttonHeight), "START"))
+		{
+			Application.LoadLevel("Level1");
+		}
+
+		if (GUI.Button(new Rect(Screen.width / 2 - (instrButtonWidth / 2), (2 * Screen.height / 3) - (buttonHeight / 2) + 80, instrButtonWidth, buttonHeight), "HOW TO PLAY"))
+		{
+			Application.LoadLevel("Instructions");
+		}
+	}
+}
