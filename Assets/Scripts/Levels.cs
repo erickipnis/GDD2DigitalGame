57bc3947dfb8346f7b365bd3f0435d74ace35b10
Lauren Reimondo <lnr3604@rit.edu>
2014-10-23 17:34:33 -0400

    Added Level changing script to the game that holds the globals for changing levels.

---
+++ Assets/Scripts/Levels.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Levels : MonoBehaviour {
+
+	public static int levelNum = 1;
+	public static int totalLevels = 2;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
